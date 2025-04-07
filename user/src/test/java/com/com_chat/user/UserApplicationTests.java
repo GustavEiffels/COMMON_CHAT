c9908e82727bd7e52636316b04f05fa300c59279
Seo Uk Sung <92835127+GustavEiffels@users.noreply.github.com>
2025-04-08 00:35:45 +0900

    [ build ]

---
+++ user/src/test/java/com/com_chat/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.com_chat.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
