e39db8c97d3dfb462655deed11de9118ab54a778
Seo Uk Sung <92835127+GustavEiffels@users.noreply.github.com>
2025-05-04 01:31:52 +0900

    ✨ feat: Create Member 생성 - 도메인 내부에서 생성 하도록 설정

--- user/src/main/java/com/com_chat/user/domain/member/Member.java
+++ user/src/main/java/com/com_chat/user/domain/member/Member.java
@@ -10,7 +10,6 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Entity
-@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(
         uniqueConstraints = {
@@ -23,16 +22,39 @@ public class Member extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long   id;
 
-
+    @Column(nullable = false)
     private String nick;
 
+    @Column(nullable = false)
     private String email; // unique
 
+    @Column(nullable = false)
     private String password;
 
     private String profilePath;
 
     private LocalDateTime lastLoginDate;
 
+
+    private Member(String nick, String email, String password, String profilePath){
+        this.nick           = nick;
+        this.email          = email;
+        this.password       = password;
+        this.profilePath     = profilePath;
+    }
+
+
+    public static Member create(String nick, String email, String password, String profilePath){
+        if( nick.isEmpty() ){
+
+        }
+        if( email.isEmpty() ){
+
+        }
+        if( password.isEmpty() ){
+
+        }
+        return new Member(nick,email,password,profilePath);
+    }
 
 }
